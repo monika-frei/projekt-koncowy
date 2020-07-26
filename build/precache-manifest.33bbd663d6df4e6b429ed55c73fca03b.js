0a369627c23ebb08811ce1a1109cc069f11ae71d
Monika <frei.monika@gmail.com>
2020-07-26 13:39:54 +0200

    gotowa strona

---
+++ build/precache-manifest.33bbd663d6df4e6b429ed55c73fca03b.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fa22523a028dc337155bffa34a161ea",
+    "url": "/projekt-koncowy/build/index.html"
+  },
+  {
+    "revision": "c3750f3340bb66a2be22",
+    "url": "/projekt-koncowy/build/static/css/2.661b9127.chunk.css"
+  },
+  {
+    "revision": "ffdfe475cb688784446b",
+    "url": "/projekt-koncowy/build/static/css/main.5509fae9.chunk.css"
+  },
+  {
+    "revision": "c3750f3340bb66a2be22",
+    "url": "/projekt-koncowy/build/static/js/2.5d389779.chunk.js"
+  },
+  {
+    "revision": "4e7896a8ce425e30bc5337b8dca3f82b",
+    "url": "/projekt-koncowy/build/static/js/2.5d389779.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffdfe475cb688784446b",
+    "url": "/projekt-koncowy/build/static/js/main.92b2b9af.chunk.js"
+  },
+  {
+    "revision": "eb2bd51be8cbb2ae29af",
+    "url": "/projekt-koncowy/build/static/js/runtime-main.9340ae82.js"
+  },
+  {
+    "revision": "baac38bb0d41d186bca61e6f84183720",
+    "url": "/projekt-koncowy/build/static/media/automotive.baac38bb.svg"
+  },
+  {
+    "revision": "aa1d850604a158e642b2758d62dbd39a",
+    "url": "/projekt-koncowy/build/static/media/bike.aa1d8506.svg"
+  },
+  {
+    "revision": "ebf2f3f4fa6920c7ea512bd4c2cca145",
+    "url": "/projekt-koncowy/build/static/media/close.ebf2f3f4.svg"
+  },
+  {
+    "revision": "5d8fa21052daf49979d26efca4ef8622",
+    "url": "/projekt-koncowy/build/static/media/close2.5d8fa210.svg"
+  },
+  {
+    "revision": "58975f7e861a777078e71bd106fee314",
+    "url": "/projekt-koncowy/build/static/media/closeImage.58975f7e.svg"
+  },
+  {
+    "revision": "bd0874fd5914bf64729dd73ced885d49",
+    "url": "/projekt-koncowy/build/static/media/double-decker-bus.bd0874fd.svg"
+  },
+  {
+    "revision": "3956a23fbade89a1f545da2f4aae9d71",
+    "url": "/projekt-koncowy/build/static/media/hitchhiker.3956a23f.svg"
+  },
+  {
+    "revision": "401396cb5acf6da7fa85b139021310aa",
+    "url": "/projekt-koncowy/build/static/media/mountain.401396cb.svg"
+  },
+  {
+    "revision": "aed6eaaa0740e0972c31a44b8313455a",
+    "url": "/projekt-koncowy/build/static/media/next.aed6eaaa.svg"
+  },
+  {
+    "revision": "9bcbcf05c78d2478ca4be382a31a4b7a",
+    "url": "/projekt-koncowy/build/static/media/plane.9bcbcf05.svg"
+  },
+  {
+    "revision": "f232c318547caada7119a8941078cdba",
+    "url": "/projekt-koncowy/build/static/media/return.f232c318.svg"
+  },
+  {
+    "revision": "772a434b24b3055fca4c19879f7da60e",
+    "url": "/projekt-koncowy/build/static/media/search-1.772a434b.svg"
+  },
+  {
+    "revision": "a4fa5905dd8eeb17c8fc416f153353ac",
+    "url": "/projekt-koncowy/build/static/media/ship.a4fa5905.svg"
+  },
+  {
+    "revision": "ec8a494dca2afa80b6f31d93bc5b76d6",
+    "url": "/projekt-koncowy/build/static/media/spaceship.ec8a494d.svg"
+  },
+  {
+    "revision": "77a77e211791a1567b71ec30a87ddc74",
+    "url": "/projekt-koncowy/build/static/media/train.77a77e21.svg"
+  }
+]);
